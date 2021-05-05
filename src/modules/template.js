c03c5eca8f0bb5490f091bfaf4fcd7fa429e0aa5
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2021-05-06 01:22:08 +0200

    Mis en forme page panier.html

--- src/modules/template.js
+++ src/modules/template.js
@@ -6,7 +6,8 @@ export var template = {
     classeLabelRadio : '<label class="labelRadio"><input type="radio" name="" class="produit-form-radio" value=""></label>',
     buttonAjouterAuPanier : '<input type="button" id="produit-form-post" value="Ajouter au Panier">',
     produitNotification : '<span id="produit-notification"></span><span id="produit-notification-goPanier"></span>',
-    counter : '<span id="counter"></span>'
+    counter : '<span id="counter"></span>',
+    productInCart: '<li class="product"><span class="product-name">Photo 50mm</span><button class="product-delete" id="" value="1" ><i class="fas fa-trash-alt"></i></button></li>'
 }   
 
 // OU et QOUI on insert
--- src/panier.js
+++ src/panier.js
@@ -1,27 +1,78 @@
 import {
     sendRequest
-} from './modules/sendRequest.js'
+} from './modules/sendRequest.js';
 
-let commande = {
-    contact: {
-        firstName: "Dmitri",
-        lastName: "Chine",
-        address: "580 Route de la Légion",
-        // {
-        //     street : ,
-        //     zipCode : "13400",
-        //     city: "Aubagne"
-        // },
-        city: "Aubagne",
-        email: "mr.dima11@gmail.com",
-    },
-     products: ["5be1ef211c9d44000030b062", "5be9bc241c9d440000a730e7", "5be1ef211c9d44000030b062"]
-}
+import {
+    quantityProduct,
+    showQuantityOfProducts
+} from './modules/cartStatus.js';
+
+import {
+    constructorHTMLCode,
+    template
+} from './modules/template.js';
+
+// let commande = {
+//     contact: {
+//         firstName: "Dmitri",
+//         lastName: "Chine",
+//         address: "580 Route de la Légion",
+//         // {
+//         //     street : ,
+//         //     zipCode : "13400",
+//         //     city: "Aubagne"
+//         // },
+//         city: "Aubagne",
+//         email: "mr.dima11@gmail.com",
+//     },
+//      products: ["5be1ef211c9d44000030b062", "5be9bc241c9d440000a730e7", "5be1ef211c9d44000030b062"]
+// }
     
 
-let link = `http://localhost:3000/api/cameras/order`;
+let linkGET = `http://localhost:3000/api/cameras/`;
+let lingPOST = `http://localhost:3000/api/cameras/order`;
+
+showQuantityOfProducts(quantityProduct());
+
+sendRequest("GET", linkGET)
+.then((data) => {
+
+    if (!quantityProduct()) {
+        document.getElementsByTagName("h2")[0].innerText = "Aucune article au panier :(";
+    } else {
+        // recuperation localStorage
+        let dataLocalStorage = JSON.parse(localStorage.getItem('produitInPanier'));
+
+        document.getElementsByTagName("h2")[0].innerText = "La composition de votre panier";
+    
+        let panierOL = document.getElementsByTagName("ol");
+        
+        for (let i = 0; i < quantityProduct(); i++) {
+            constructorHTMLCode(panierOL[0], template.productInCart);
+            let objetJSON = data.find(objet => objet._id === dataLocalStorage.product_id[i]);
+
+            // regarder si c'est lenses / couler / vernil
+            let option = JSON.stringify(objetJSON).slice(2, JSON.stringify(objetJSON).indexOf('":'));
+
+            console.log("objet " + objetJSON.lenses);
+            document.getElementsByClassName("product-name")[i].innerText = objetJSON.name + " " + option + " " + objetJSON.lenses;
+
+            document.getElementsByClassName("product-delete")[i].setAttribute("onclick", "");
+        }
+       
+        let deleteButton = document.getElementsByClassName("product-delete");
+        deleteButton.onclick(() => alert("gogogo"))
+
+    }
+
+})
 
 
-sendRequest('POST', link, commande)
-    .then(data => console.log(data.orderId))
-    .catch(err => console.log(err))
+
+
+
+// sendRequest('POST', link, commande)
+//     .then(data => {
+//         console.log(data.orderId)
+//     })
+//     .catch(err => console.log(err))
