1f2e80e9a26108a1522cb6a5b7c703cf2c8b5f81
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2021-05-15 01:26:38 +0200

    Fin de developpement, cinfiguration hostLink.js

--- src/modules/hostLink.js
+++ src/modules/hostLink.js
@@ -1,5 +1,5 @@
 export const portFronEnd = ':8080';
 export const portServer = ':3000';
-export const domaine = ("http://" + location.hostname);
+export const domaine = ("http://" + "localhost");
 export const link = (domaine + portServer + '/api/cameras');
 export const linkPOST = (domaine + portServer + '/api/cameras/order');
