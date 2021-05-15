d321593bebdb653fac7cda1ee6eba85f9f62808c
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2021-05-15 12:57:56 +0200

    Deploy to heroku pour gitpage

--- src/modules/hostLink.js
+++ src/modules/hostLink.js
@@ -1,5 +1,5 @@
 export const portFronEnd = ':8080';
 export const portServer = ':3000';
-export const domaine = ("http://" + "localhost");
-export const link = (domaine + portServer + '/api/cameras');
-export const linkPOST = (domaine + portServer + '/api/cameras/order');
+export const domaine = ("http://" + "nodejs-for-orinoco.herokuapp.com");
+export const link = (domaine + '/api/cameras');
+export const linkPOST = (domaine  + '/api/cameras/order');
