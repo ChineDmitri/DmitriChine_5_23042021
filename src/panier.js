5d69432c94231def1b3fd7f6b874ea3e9886aef0
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2021-05-22 22:15:04 +0200

    Creation url avec prix pour redirect sur la page confirmatio,.html

--- src/panier.js
+++ src/panier.js
@@ -199,9 +199,20 @@ sendRequest("GET", link)
                 // send request POST et recuperation in preview orderId et redirect vers page confirmation
                 sendRequest('POST', linkPOST, commande)
                     .then((data) => {
+                        // recuperation de localstorage 
+                        let bufferLocalStorage = JSON.parse(localStorage.getItem('produitInPanier'));
+
+                        // prix total
+                        let prixTotal = 0
+                        for (let i = 0; i < quantityProduct(); i++) {
+                            let prixTypeOfNumber = Number(bufferLocalStorage.prix[i])
+                            prixTotal += prixTypeOfNumber;
+                        }
+                        prixTotal *= puissance;
+                        
                         console.log("Votre id de comande", data.orderId);
-                        window.location.href = (domaine + portFronEnd + "/confirmation.html?id=" + data.orderId);
-                        localStorage.removeItem("produitInPanier");
+                        window.location.href = (domaine + portFronEnd + "/confirmation.html?id=" + data.orderId + "&prix=" + prixTotal);
+                        // localStorage.removeItem("produitInPanier");
                     })
 
                 console.log("votre commande", commande)
